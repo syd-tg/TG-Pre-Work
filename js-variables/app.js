23e96f97d3c49a1ffdccb41ec2dd3ec91abe9579
syd-tg <zaphappier@gmail.com>
2018-08-28 16:23:01 -0600

    adds work from exercise 7 (String Interpolation II) of JS - Variables

--- js-variables/app.js
+++ js-variables/app.js
@@ -1,2 +1,5 @@
-let favoriteAnimal = 'cat';
-console.log('My favorite animal: ' + favoriteAnimal);
+let myName = 'Sydney';
+let myCity = 'Sydney';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+//Koeln, Germany is my real favorite city, just thought this was funny.
