49e9e74f95c5d5953d2c573dcd956e05edcebf7f
syd-tg <42363268+syd-tg@users.noreply.github.com>
2018-08-28 16:24:19 -0600

    adds work from JS - Variables tutorial

---
+++ js-variables/app.js
@@ -0,0 +1,5 @@
+let myName = 'Sydney';
+let myCity = 'Sydney';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+//Koeln, Germany is my real favorite city, just thought this was funny.
